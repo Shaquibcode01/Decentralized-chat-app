32f3853d6430a55debdee139a6875613068b9b02
Shaquibkhan <shaquibkhan2003@gmail.com>
2024-06-24 00:33:48 +0530

    Your commit message

---
+++ Components/Filter/Filter.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useContext } from "react";
+import Image from "next/image";
+
+//INTERNAL IMPORT
+import Style from "./Filter.module.css";
+import images from "../../assets";
+import { ChatAppContect } from "../../Context/ChatAppContext";
+import { Model } from "../index";
+
+const Filter = () => {
+  const { account, addFriends } = useContext(ChatAppContect);
+
+  //USESTATE
+  const [addFriend, setAddFriend] = useState(false);
+  return (
+    <div className={Style.Filter}>
+      <div className={Style.Filter_box}>
+        <div className={Style.Filter_box_right}>
+          <button onClick={() => setAddFriend(true)}>
+            <Image src={images.user} alt="clear" width={20} height={20} />
+            Add Friend
+          </button>
+        </div>
+      </div>
+
+      {/* //MODEL COMPONENT */}
+      {addFriend && (
+        <div className={Style.Filter_model}>
+          <Model
+            openBox={setAddFriend}
+            title="Welcome To"
+            head="Decentralized Chat App"
+            info="Say NO to censorship, data leaks, privacy breach and centralised control, say YES to Web3.0, privacy, end to end security and decentralisation"
+            smallInfo="Please fill in your details...."
+            image={images.hero}
+            functionName={addFriends}
+          />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Filter;
